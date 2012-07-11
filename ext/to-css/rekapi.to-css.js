00c121519a2ec127c9f45f76d5ae29833fc74ba5
Jeremy Kahn <jeremyckahn@gmail.com>
2012-07-10 22:33:06 -0700

    Separates CSS tracks.

--- ext/to-css/rekapi.to-css.js
+++ ext/to-css/rekapi.to-css.js
@@ -137,24 +137,27 @@ var rekapiToCSS = function (context, _) {
   function generateBoilerplatedKeyframes (
       actor, animName, granularity, opt_vendors) {
 
+    var trackNames = _.keys(actor._propertyTracks);
+    var trackNames = actor.getTrackNames();
     var optimizedEasingFormula = getOptimizedEasingFormula(actor);
-    var keyframes;
+    var cssTracks = [];
 
     // TODO: CSS optimization is _extremely_ incomplete.  It only supports
     // single-step animations with one keyframe property.
     if (typeof optimizedEasingFormula === 'string') {
-      keyframes = generateOptimizedKeyframes(actor, optimizedEasingFormula);
+      cssTracks = [generateOptimizedKeyframes(actor, optimizedEasingFormula)];
     } else {
-      keyframes = generateActorKeyframes(actor, granularity);
+      _.each(trackNames, function (trackName) {
+        cssTracks.push(
+          generateActorKeyframes(actor, granularity, trackName));
+      });
     }
 
     var boilerplatedKeyframes = [];
-    var trackNames = _.keys(actor._propertyTracks);
-    var trackNames = actor.getTrackNames();
 
-    _.each(trackNames, function (trackName) {
+    _.each(trackNames, function (trackName, i) {
       boilerplatedKeyframes.push(applyVendorBoilerplates(
-        keyframes, (animName + '-' + trackName), opt_vendors));
+        cssTracks[i], (animName + '-' + trackName), opt_vendors));
     });
 
     boilerplatedKeyframes = boilerplatedKeyframes.join('\n');
